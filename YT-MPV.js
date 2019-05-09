af9ce882ae787f088a99ee595391af2edb30a397
drdrago5 <34965336+drdrago5@users.noreply.github.com>
2019-05-09 21:11:21 +0300

    Update YT-MPV.js

--- YT-MPV.js
+++ YT-MPV.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         YT MPV
+// @name         Taka Blyat Sraka
 // @version      1.0
 // @description  Adds an Open in MPV button to YouTube
 // @include      http*://*.youtube.com/*
@@ -11,37 +11,43 @@
 
 function polymerInject() {
   /* Create button */
+  var my_awesome_script = document.createElement('script');
+  my_awesome_script.setAttribute('src','https://use.fontawesome.com/releases/v5.8.2/js/all.js');
+  my_awesome_script.setAttribute('integrity','sha384-DJ25uNYET2XCl5ZF++U8eNxPWqcKohUUBUpKGlNLMchM7q4Wjg2CUpjHLaL8yYPH');
+  my_awesome_script.setAttribute('crossorigin', 'anonymous');
+  document.head.appendChild(my_awesome_script);
   var btnDiv = document.createElement("div");
   btnDiv.style.width = "100%";
   btnDiv.id = "parentButton";
   var btn = document.createElement("a");
   btn.style.display = "block";
   btn.style.width = "100%";
-  btn.style.backgroundColor = "#181717";
-  btn.style.color = "white";
+  btn.style.backgroundColor = "#121212";
+  btn.style.color = "#909090";
   btn.style.textAlign = "center";
   btn.style.padding = "10px 0";
   btn.style.marginTop = "5px";
   btn.style.fontSize = "14px";
-  btn.style.border = "0";
+  btn.style.border = "1px";
+  btn.style.borderColor = "#909090";
   btn.style.cursor = "pointer";
-  btn.style.textDecoration = "none";
   btn.style.borderRadius = "2px";
   btn.style.fontFamily = "Roboto, Arial, sans-serif";
   btn.href = "mpv://" + window.location.href;
-  btn.appendChild(document.createTextNode("Open in MPV"));
   btnDiv.appendChild(btn);
-
+  var img = document.createElement("i");
+  img.setAttribute('Class', 'far fa-play-circle fa-2x');
+  btn.appendChild(img);
   /* Find and add to target */
-  var te = document.querySelectorAll("[id='subscribe-button']");
+  var te = document.querySelectorAll("[id='notification-preference-toggle-button']");
   for (var i = 0; i < te.length; i++) {
-    if (te[i].className.indexOf("ytd-video-secondary-info-renderer") > -1) {
+    if (te[i].className.indexOf("style-scope ytd-subscribe-button-renderer") > -1) {
       te[i].appendChild(btnDiv);
     }
   }
 
   /* Fix hidden description bug */
-  var dscBox = document.querySelectorAll("ytd-video-secondary-info-renderer");
+  var dscBox = document.querySelectorAll("style-scope ytd-subscribe-button-renderer");
   if (dscBox[0].className.indexOf("loading") > -1){
     dscBox[0].classList.remove("loading");
   }
